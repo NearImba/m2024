4213e010cf32db31386a07c02132c1750ce9334e
nearimba <yu.mei@sunmi.com>
2024-12-12 19:59:17 +0800

    feat: add workspaces

--- src/lib/types.d.ts
+++
@@ -1,5 +0,0 @@
-export type ActorData = {
-    id: string;
-    name: string;
-    age: number;
-};
--- src/routes/editor/[pid]/$types.d.ts
+++
@@ -1,2 +0,0 @@
-export type PageLoad = (args: { params: { pid: string } }) => { pid: string };
-export type PageData = ReturnType<PageLoad>;
